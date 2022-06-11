9e4b2465449a64cbf11ffb726b1a318818719f0c
Naoufal <n.elyousfi@gmail.com>
2022-06-11 13:37:17 +0100

    Add valtio

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { proxy, useSnapshot } from "valtio";
 
-function App() {
+const state = proxy({ count: 0 });
+
+const App = () => {
+  const snap = useSnapshot(state);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{snap.count}</h1>
+      <button onClick={() => ++state.count}>Increment</button>
+    </>
   );
-}
+};
 
 export default App;
